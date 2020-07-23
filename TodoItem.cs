fc76e7fc5423510a33bdc48499afa41c55749688
trevor <trevor.winger@artunlimitedusa.com>
2020-07-23 08:51:26 -0500

    initial commit

---
+++ TodoItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestProgressiveWebApp
+{
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public bool isDone { get; set; }
+
+        public TodoItem(string title, bool done)
+        {
+            Title = title;
+            isDone = done;
+        }
+    }
+}
